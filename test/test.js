7b96e509a3c342fe493416a2612c4004144647f7
punthawee <tumpcknan@gmail.com>
2024-09-16 00:07:29 +0700

    Refactor: Split data transformation functions into separate files and update index.js to import and export functions. Added test script for camelToSnake function.

--- test/test.js
+++ test/test.js
@@ -1,22 +1,19 @@
 
-const DataTransformer = require('p-transfom');
-
-// Create an instance of DataTransformer
-const transformer = new DataTransformer();
-
+const {camelToSnake} = require('p-transfom');
+ 
 // Test data
 const testData = {
   firstName: 'John',
   lastName: 'Doe',
   contactDetails: {
-    emailAddress: 'john.doe@example.com',
-    phoneNumber: '123-456-7890'
+      emailAddress: 'john.doe@example.com',
+      phoneNumber: '123-456-7890'
   },
   hobbies: ['readingBooks', 'playingGuitar']
 };
 
 // Convert camelCase to snake_case
-const result = transformer.camelToSnake(testData);
+const result = camelToSnake(testData);
 
 // Log the result
 console.log('Original Data:', testData);
